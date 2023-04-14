f78f7638abdf2a315e356f889aabdaf47dd3c3dc
Kiyarash <kiarashnadri@yahoo.com>
2023-04-14 11:24:47 +0330

    edit style of Cart icon

--- src/App.js
+++ src/App.js
@@ -75,8 +75,9 @@ function App() {
         <Route exact path="/">
           <main>
             <Tours tours={tours} removeTours={removeTours} addTours={addTours} />
-            <Link to="/Cart" className='myLink'>You have {cartTours.length} tours in Cart
+            <Link to="/Cart" className='myLink'>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none" /><path d="M184,184H69.8L41.9,30.6A8,8,0,0,0,34.1,24H16" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="8" /><circle cx="80" cy="204" r="20" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="8" /><circle cx="184" cy="204" r="20" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="8" /><path d="M62.5,144H188.1a15.9,15.9,0,0,0,15.7-13.1L216,64H48" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="8" /></svg>
+              <span className='numberOfTour'>{cartTours.length}</span>
             </Link>
           </main>
         </Route>
